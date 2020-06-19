d59b8a11ddeed2499f29e86c8c92bf1bcf2a7879
William Chen <13495049+CPWstatic@users.noreply.github.com>
2020-06-19 14:13:47 +0800

    Address comment.

--- src/schedule/Scheduler.cpp
+++ src/schedule/Scheduler.cpp
@@ -147,8 +147,7 @@ folly::Future<Status> Scheduler::iterate(LoopExecutor *loop) {
         if (!val.isBool()) {
             std::stringstream ss;
             ss << "Loop produces a bad condition result: " << val << " type: " << val.type();
-            LOG(ERROR) << ss.str();
-            return folly::makeFuture(Status::Error(ss.str()));
+            return loop->error(Status::Error(ss.str()));
         }
         auto cond = val.moveBool();
         if (!cond) return folly::makeFuture(Status::OK());
