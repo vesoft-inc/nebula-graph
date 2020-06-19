1d6240c5dd7398c86410da94ffa47738a5f8ab3f
yixinglu <2520865+yixinglu@users.noreply.github.com>
2020-06-19 10:33:52 +0800

    Add ValidatorTestBase

--- src/validator/test/ValidatorTest.cpp
+++ src/validator/test/ValidatorTest.cpp
@@ -6,8 +6,7 @@
 
 #include "common/base/Base.h"
 
-#include <gtest/gtest.h>
-
+#include "validator/test/ValidatorTestBase.h"
 #include "parser/GQLParser.h"
 #include "validator/ASTValidator.h"
 #include "context/QueryContext.h"
@@ -22,7 +21,7 @@
 namespace nebula {
 namespace graph {
 
-class ValidatorTest : public ::testing::Test {
+class ValidatorTest : public ValidatorTestBase {
 public:
     static void SetUpTestCase() {
         auto session = new ClientSession(0);
@@ -38,107 +37,6 @@ public:
     }
 
     std::unique_ptr<QueryContext> buildContext();
-
-protected:
-    ::testing::AssertionResult verifyPlan(const PlanNode* root,
-                                          const std::vector<PlanNode::Kind>& expected) const {
-        if (root == nullptr) {
-            return ::testing::AssertionFailure() << "Get nullptr plan.";
-        }
-
-        std::vector<PlanNode::Kind> result;
-        bfsTraverse(root, result);
-        if (result == expected) {
-            return ::testing::AssertionSuccess();
-        } else {
-            return ::testing::AssertionFailure() << "\n     Result: " << printPlan(result)
-                                                 << "\n     Expected: " << printPlan(expected);
-        }
-    }
-
-    std::string printPlan(const std::vector<PlanNode::Kind>& plan) const {
-        std::vector<const char*> kinds;
-        kinds.reserve(plan.size());
-        std::transform(plan.cbegin(), plan.cend(), kinds.begin(), PlanNode::toString);
-        std::stringstream ss;
-        ss << "[" << folly::join(", ", kinds) << "]";
-        return ss.str();
-    }
-
-    void bfsTraverse(const PlanNode* root, std::vector<PlanNode::Kind>& result) const {
-        std::queue<const PlanNode*> queue;
-        std::unordered_set<int64_t> visited;
-        queue.emplace(root);
-
-        while (!queue.empty()) {
-            auto node = queue.front();
-            queue.pop();
-            if (visited.find(node->id()) != visited.end()) {
-                continue;
-            }
-            visited.emplace(node->id());
-            result.emplace_back(node->kind());
-
-            switch (node->kind()) {
-                case PlanNode::Kind::kUnknown:
-                    ASSERT_TRUE(false) << "Unknown Plan Node.";
-                case PlanNode::Kind::kStart: {
-                    break;
-                }
-                case PlanNode::Kind::kGetNeighbors:
-                case PlanNode::Kind::kGetVertices:
-                case PlanNode::Kind::kGetEdges:
-                case PlanNode::Kind::kReadIndex:
-                case PlanNode::Kind::kFilter:
-                case PlanNode::Kind::kProject:
-                case PlanNode::Kind::kSort:
-                case PlanNode::Kind::kLimit:
-                case PlanNode::Kind::kAggregate:
-                case PlanNode::Kind::kSwitchSpace:
-                case PlanNode::Kind::kMultiOutputs:
-                case PlanNode::Kind::kDedup: {
-                    auto* current = static_cast<const SingleInputNode*>(node);
-                    queue.emplace(current->input());
-                    break;
-                }
-                case PlanNode::Kind::kCreateSpace:
-                case PlanNode::Kind::kCreateTag:
-                case PlanNode::Kind::kCreateEdge:
-                case PlanNode::Kind::kDescSpace:
-                case PlanNode::Kind::kDescTag:
-                case PlanNode::Kind::kDescEdge:
-                case PlanNode::Kind::kInsertVertices:
-                case PlanNode::Kind::kInsertEdges: {
-                    // TODO: DDLs and DMLs are kind of single input node.
-                }
-                case PlanNode::Kind::kUnion:
-                case PlanNode::Kind::kIntersect:
-                case PlanNode::Kind::kMinus: {
-                    auto* current = static_cast<const BiInputNode*>(node);
-                    queue.emplace(current->left());
-                    queue.emplace(current->right());
-                    break;
-                }
-                case PlanNode::Kind::kSelector: {
-                    auto* current = static_cast<const Selector*>(node);
-                    queue.emplace(current->input());
-                    queue.emplace(current->then());
-                    if (current->otherwise() != nullptr) {
-                        queue.emplace(current->otherwise());
-                    }
-                    break;
-                }
-                case PlanNode::Kind::kLoop: {
-                    auto* current = static_cast<const Loop*>(node);
-                    queue.emplace(current->input());
-                    queue.emplace(current->body());
-                    break;
-                }
-                default:
-                    LOG(FATAL) << "Unknown PlanNode: " << static_cast<int64_t>(node->kind());
-            }
-        }
-    }
 
 protected:
     static std::shared_ptr<ClientSession>      session_;
