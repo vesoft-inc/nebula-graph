dfc9c38df260f5f52dcc503fb3f85beca235c345
yixinglu <2520865+yixinglu@users.noreply.github.com>
2020-07-13 17:06:32 +0800

    Fix storage cmake configure parameter

--- cmake/AddDependentProject.cmake
+++ cmake/AddDependentProject.cmake
@@ -6,40 +6,40 @@
 
 macro(add_dependent_project)
     cmake_parse_arguments(
-        dep_proj_args               # prefix
+        DEP_PROJ                    # prefix
         ""                          # <options>
         "BASE;NAME;REPO;TAG"        # <one_value_args>
         ""                          # <multi_value_args>
         ${ARGN}
     )
 
-    set(CLONE_DIR ${dep_proj_args_BASE}/${dep_proj_args_NAME})
+    set(CLONE_DIR ${DEP_PROJ_BASE}/${DEP_PROJ_NAME})
     # Clone or update the repo
     if(EXISTS ${CLONE_DIR}/.git)
-        message(STATUS "Updating from the repo \"" ${dep_proj_args_REPO} "\"")
+        message(STATUS "Updating from the repo \"" ${DEP_PROJ_REPO} "\"")
         execute_process(
             COMMAND ${GIT_EXECUTABLE} pull --depth=1
             WORKING_DIRECTORY ${CLONE_DIR}
             RESULT_VARIABLE clone_result
         )
     else()
-        message(STATUS "Cloning from the repo \"" ${dep_proj_args_REPO} "\"")
+        message(STATUS "Cloning from the repo \"" ${DEP_PROJ_REPO} "\"")
         execute_process(
             COMMAND ${GIT_EXECUTABLE} clone
                 --depth 1
                 --progress
                 --single-branch
-                --branch ${dep_proj_args_TAG}
-                ${dep_proj_args_REPO}
+                --branch ${DEP_PROJ_TAG}
+                ${DEP_PROJ_REPO}
                 ${CLONE_DIR}
             RESULT_VARIABLE clone_result
         )
     endif()
 
     if(NOT ${clone_result} EQUAL 0)
-        message(FATAL_ERROR "Cannot clone the repo from \"${dep_proj_args_REPO}\" (branch \"${dep_proj_args_TAG}\"): \"${clone_result}\"")
+        message(FATAL_ERROR "Cannot clone the repo from \"${DEP_PROJ_REPO}\" (branch \"${DEP_PROJ_TAG}\"): \"${clone_result}\"")
     else()
-        message(STATUS "Updated the repo from \"${dep_proj_args_REPO}\" (branch \"${dep_proj_args_TAG}\")")
+        message(STATUS "Updated the repo from \"${DEP_PROJ_REPO}\" (branch \"${DEP_PROJ_TAG}\")")
     endif()
 
     # Configure the repo
@@ -57,19 +57,19 @@ macro(add_dependent_project)
             -DENABLE_CCACHE=${ENABLE_CCACHE}
             -DENABLE_ASAN=${ENABLE_ASAN}
             -DENABLE_UBSAN=${ENABLE_UBSAN}
-            ${dep_proj_args_ARGN}
+            ${DEP_PROJ_UNPARSED_ARGUMENTS}
             .
         WORKING_DIRECTORY ${CLONE_DIR}
         RESULT_VARIABLE cmake_result
     )
     if(NOT ${cmake_result} EQUAL 0)
-        message(FATAL_ERROR "Failed to configure the dependent project \"" ${dep_proj_args_NAME} "\"")
+        message(FATAL_ERROR "Failed to configure the dependent project \"" ${DEP_PROJ_NAME} "\"")
     endif()
 
     # Add a custom target to build the project
     add_custom_target(
-        ${dep_proj_args_NAME}_project ALL
-        COMMAND make -j
+        ${DEP_PROJ_NAME}_project ALL
+        COMMAND make -j $(nproc)
         WORKING_DIRECTORY ${CLONE_DIR}
     )
 
