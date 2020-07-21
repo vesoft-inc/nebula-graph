fcd4327323c12545af38dcd6bf390420fd79c30b
jimingquan <mingquan.ji@vesoft.com>
2020-07-21 15:04:19 +0800

    add case

--- tests/query/stateless/test_new_go.py
+++ tests/query/stateless/test_new_go.py
@@ -21,7 +21,7 @@ class TestGoQuery(PrepareData):
         }
 
         stmt = '''GO FROM "Boris Diaw" OVER serve YIELD \
-        $^.player.name , serve.start_year, serve.end_year, $$.team.name'''
+            $^.player.name , serve.start_year, serve.end_year, $$.team.name'''
         resp = self.execute_query(stmt)
         self.check_resp_succeeded(resp)
         expected_data = {
@@ -32,8 +32,8 @@ class TestGoQuery(PrepareData):
         }
 
         stmt = '''GO FROM "Rajon Rondo" OVER serve WHERE \
-        serve.start_year >= 2013 && serve.end_year <= 2018 YIELD \
-        $^.player.name, serve.start_year, serve.end_year, $$.team.name'''
+            serve.start_year >= 2013 && serve.end_year <= 2018 YIELD \
+            $^.player.name, serve.start_year, serve.end_year, $$.team.name'''
         resp = self.execute_query(stmt)
         self.check_resp_succeeded(resp)
         expected_data = {
@@ -44,7 +44,7 @@ class TestGoQuery(PrepareData):
         }
 
         stmt = '''GO FROM "Boris Diaw" OVER like YIELD like._dst as id \
-        | GO FROM $-.id OVER like YIELD like._dst as id | GO FROM $-.id OVER serve'''
+            | GO FROM $-.id OVER like YIELD like._dst as id | GO FROM $-.id OVER serve'''
         resp = self.execute_query(stmt)
         self.check_resp_succeeded(resp)
         expected_data = {
@@ -55,7 +55,7 @@ class TestGoQuery(PrepareData):
         }
 
         stmt = '''GO FROM "Boris Diaw" OVER like YIELD like._dst as id \
-        | ( GO FROM $-.id OVER like YIELD like._dst as id | GO FROM $-.id OVER serve )'''
+            | ( GO FROM $-.id OVER like YIELD like._dst as id | GO FROM $-.id OVER serve )'''
         resp = self.execute_query(stmt)
         self.check_resp_succeeded(resp)
         expected_data = {
@@ -77,7 +77,7 @@ class TestGoQuery(PrepareData):
 
     def test_assignment_simple(self):
         stmt = '''$var = GO FROM "Tracy McGrady" OVER like YIELD like._dst as id; \
-        GO FROM $var.id OVER like'''
+            GO FROM $var.id OVER like'''
         resp = self.execute_query(stmt)
         self.check_resp_succeeded(resp)
         expected_data = {
@@ -89,8 +89,8 @@ class TestGoQuery(PrepareData):
 
     def test_assignment_pipe(self):
         stmt = '''$var = (GO FROM "Tracy McGrady" OVER like YIELD like._dst as id \
-        | GO FROM $-.id OVER like YIELD like._dst as id); \
-        GO FROM $var.id OVER like'''
+            | GO FROM $-.id OVER like YIELD like._dst as id); \
+            GO FROM $var.id OVER like'''
         resp = self.execute_query(stmt)
         self.check_resp_succeeded(resp)
         expected_data = {
@@ -102,7 +102,7 @@ class TestGoQuery(PrepareData):
 
     def test_assignment_empty_result(self):
         stmt = '''$var = GO FROM "-1" OVER like YIELD like._dst as id; \
-        GO FROM $var.id OVER like'''
+            GO FROM $var.id OVER like'''
         resp = self.execute_query(stmt)
         self.check_resp_succeeded(resp)
         expected_data = {
@@ -119,13 +119,13 @@ class TestGoQuery(PrepareData):
 
     def test_distinct(self):
         stmt = '''GO FROM "Nobody" OVER serve \
-        YIELD DISTINCT $^.player.name as name, $$.team.name as name'''
+            YIELD DISTINCT $^.player.name as name, $$.team.name as name'''
         resp = self.execute_query(stmt)
         self.check_resp_succeeded(resp)
 
         stmt = '''GO FROM "Boris Diaw" OVER like YIELD like._dst as id \
-        | GO FROM $-.id OVER like YIELD like._dst as id | GO FROM $-.id OVER serve \
-        YIELD DISTINCT serve._dst, $$.team.name'''
+            | GO FROM $-.id OVER like YIELD like._dst as id | GO FROM $-.id OVER serve \
+            YIELD DISTINCT serve._dst, $$.team.name'''
         resp = self.execute_query(stmt)
         self.check_resp_succeeded(resp)
         expected_data = {
@@ -157,7 +157,7 @@ class TestGoQuery(PrepareData):
         }
 
         stmt = '''GO FROM "NON EXIST VERTEX ID" OVER serve YIELD \
-        $^.player.name, serve.start_year, serve.end_year, $$team.name'''
+            $^.player.name, serve.start_year, serve.end_year, $$team.name'''
         resp = self.execute_query(stmt)
         self.check_resp_succeeded(resp)
         expected_data = {
@@ -168,7 +168,7 @@ class TestGoQuery(PrepareData):
         }
 
         stmt = '''GO FROM "NON EXIST VERTEX ID" OVER serve YIELD DISTINCT \
-        $^.player.name, serve.start_year, serve.end_year, $$team.name'''
+            $^.player.name, serve.start_year, serve.end_year, $$team.name'''
         resp = self.execute_query(stmt)
         self.check_resp_succeeded(resp)
         expected_data = {
@@ -190,7 +190,7 @@ class TestGoQuery(PrepareData):
         }
 
         stmt = '''GO FROM "Russell Westbrook" OVER serve, like REVERSELY \
-        YIELD serve._dst, like._dst, serve._type, like._type'''
+            YIELD serve._dst, like._dst, serve._type, like._type'''
         resp = self.execute_query(stmt)
         self.check_resp_succeeded(resp)
         expected_data = {
@@ -241,7 +241,7 @@ class TestGoQuery(PrepareData):
         }
 
         stmt = '''GO FROM "Manu Ginobili" OVER like, teammate REVERSELY YIELD like.likeness, \
-        teammate.start_year, $$.player.name'''
+            teammate.start_year, $$.player.name'''
         resp = self.execute_query(stmt)
         self.check_resp_succeeded(resp)
         expected_data = {
@@ -252,7 +252,7 @@ class TestGoQuery(PrepareData):
         }
 
         stmt = '''GO FROM "Russell Westbrook" OVER serve, like \
-        YIELD serve.start_year, like.likeness, serve._type, like._type'''
+            YIELD serve.start_year, like.likeness, serve._type, like._type'''
         resp = self.execute_query(stmt)
         self.check_resp_succeeded(resp)
         expected_data = {
@@ -303,7 +303,7 @@ class TestGoQuery(PrepareData):
         }
 
         stmt = '''GO FROM "Boris Diaw" OVER like, serve YIELD like._dst as id \
-        | ( GO FROM $-.id OVER like YIELD like._dst as id | GO FROM $-.id OVER serve )'''
+            | ( GO FROM $-.id OVER like YIELD like._dst as id | GO FROM $-.id OVER serve )'''
         resp = self.execute_query(stmt)
         self.check_resp_succeeded(resp)
         expected_data = {
@@ -314,11 +314,151 @@ class TestGoQuery(PrepareData):
         }
 
         stmt = '''GO FROM "Boris Diaw" OVER * YIELD like._dst as id \
-        | ( GO FROM $-.id OVER like YIELD like._dst as id | GO FROM $-.id OVER serve )'''
+            | ( GO FROM $-.id OVER like YIELD like._dst as id | GO FROM $-.id OVER serve )'''
         resp = self.execute_query(stmt)
         self.check_resp_succeeded(resp)
         expected_data = {
             column_names : [],
+            rows : [
+                []
+            ]
+        }
+
+    def test_reference_pipein_yieldandwhere(self):
+        stmt = '''GO FROM 'Tim Duncan', 'Chris Paul' OVER like \
+            YIELD $^.player.name AS name, like._dst AS id \
+            | GO FROM $-.id OVER like \
+            YIELD $-.name, $^.player.name, $$.player.name'''
+        resp = self.execute_query(stmt)
+        self.check_resp_succeeded(resp)
+        expected_data = {
+            column_names : ["$-.name", "$^.player.name", "$$.player.name"],
+            rows : [
+                ["Tim Duncan", "Manu Ginobili", "Tim Duncan"],
+                ["Tim Duncan", "Tony Parker", "LaMarcus Aldridge"],
+                ["Tim Duncan", "Tony Parker", "Manu Ginobili"],
+                ["Tim Duncan", "Tony Parker", "Tim Duncan"],
+                ["Chris Paul", "LeBron James", "Ray Allen"],
+                ["Chris Paul", "Carmelo Anthony", "Chris Paul"],
+                ["Chris Paul", "Carmelo Anthony", "LeBron James"],
+                ["Chris Paul", "Carmelo Anthony", "Dwyane Wade"],
+                ["Chris Paul", "Dwyane Wade", "Chris Paul"],
+                ["Chris Paul", "Dwyane Wade", "LeBron James"],
+                ["Chris Paul", "Dwyane Wade", "Carmelo Anthony"]
+            ]
+        }
+
+        stmt = '''GO FROM 'Tim Duncan', 'Chris Paul' OVER like \
+            YIELD $^.player.name AS name, like._dst AS id \
+            | GO FROM $-.id OVER like \
+            WHERE $-.name != $$.player.name \
+            YIELD $-.name, $^.player.name, $$.player.name'''
+        resp = self.execute_query(stmt)
+        self.check_resp_succeeded(resp)
+        expected_data = {
+            column_names : ["$-.name", "$^.player.name", "$$.player.name"],
+            rows : [
+                ["Tim Duncan", "Tony Parker", "LaMarcus Aldridge"],
+                ["Tim Duncan", "Tony Parker", "Manu Ginobili"],
+                ["Chris Paul", "LeBron James", "Ray Allen"],
+                ["Chris Paul", "Carmelo Anthony", "LeBron James"],
+                ["Chris Paul", "Carmelo Anthony", "Dwyane Wade"],
+                ["Chris Paul", "Dwyane Wade", "LeBron James"],
+                ["Chris Paul", "Dwyane Wade", "Carmelo Anthony"]
+            ]
+        }
+
+        stmt = '''GO FROM 'Tim Duncan', 'Chris Paul' OVER like \
+            YIELD $^.player.name AS name, like._dst AS id \
+            | GO FROM $-.id OVER like \
+            YIELD $-.*, $^.player.name, $$.player.name'''
+        resp = self.execute_query(stmt)
+        self.check_resp_succeeded(resp)
+        expected_data = {
+            column_names : ["$-.name", "$^.player.name", "$$.player.name"],
+            rows : [
+                []
+            ]
+        }
+
+    def test_reference_variable_in_yieldandwhere(self):
+        stmt = '''$var = GO FROM 'Tim Duncan', 'Chris Paul' OVER like \
+            YIELD $^.player.name AS name, like._dst AS id; \
+            GO FROM $var.id OVER like \
+            YIELD $var.name, $^.player.name, $$.player.name'''
+        resp = self.execute_query(stmt)
+        self.check_resp_succeeded(resp)
+        expected_data = {
+            column_names : ["$var.name", "$^.player.name", "$$.player.name"],
+            rows : [
+                ["Tim Duncan", "Manu Ginobili", "Tim Duncan"],
+                ["Tim Duncan", "Tony Parker", "LaMarcus Aldridge"],
+                ["Tim Duncan", "Tony Parker", "Manu Ginobili"],
+                ["Tim Duncan", "Tony Parker", "Tim Duncan"],
+                ["Chris Paul", "LeBron James", "Ray Allen"],
+                ["Chris Paul", "Carmelo Anthony", "Chris Paul"],
+                ["Chris Paul", "Carmelo Anthony", "LeBron James"],
+                ["Chris Paul", "Carmelo Anthony", "Dwyane Wade"],
+                ["Chris Paul", "Dwyane Wade", "Chris Paul"],
+                ["Chris Paul", "Dwyane Wade", "LeBron James"],
+                ["Chris Paul", "Dwyane Wade", "Carmelo Anthony"]
+            ]
+        }
+
+        stmt = '''$var = GO FROM 'Tim Duncan', 'Chris Paul' OVER like \
+            YIELD $^.player.name AS name, like._dst AS id; \
+            GO FROM $var.id OVER like \
+            WHERE $var.name != $$.player.name \
+            YIELD $var.name, $^.player.name, $$.player.name'''
+        resp = self.execute_query(stmt)
+        self.check_resp_succeeded(resp)
+        expected_data = {
+            column_names : ["$var.name", "$^.player.name", "$$.player.name"],
+            rows : [
+                ["Tim Duncan", "Tony Parker", "LaMarcus Aldridge"],
+                ["Tim Duncan", "Tony Parker", "Manu Ginobili"],
+                ["Chris Paul", "LeBron James", "Ray Allen"],
+                ["Chris Paul", "Carmelo Anthony", "LeBron James"],
+                ["Chris Paul", "Carmelo Anthony", "Dwyane Wade"],
+                ["Chris Paul", "Dwyane Wade", "LeBron James"],
+                ["Chris Paul", "Dwyane Wade", "Carmelo Anthony"]
+            ]
+        }
+
+        stmt = '''$var = GO FROM 'Tim Duncan', 'Chris Paul' OVER like \
+            YIELD $^.player.name AS name, like._dst AS id; \
+            GO FROM $var.id OVER like \
+            YIELD $var.*, $^.player.name, $$.player.name'''
+        resp = self.execute_query(stmt)
+        self.check_resp_succeeded(resp)
+        expected_data = {
+            column_names : ["$var.name", "$^.player.name", "$$.player.name"],
+            rows : [
+                []
+            ]
+        }
+
+    def test_no_existent_prop(self):
+        stmt = "GO FROM 'Tim Duncan' OVER serve YIELD $^.player.test"
+        resp = self.execute_query(stmt)
+        self.check_resp_failed(resp)
+
+        stmt = "GO FROM 'Tim Duncan' OVER serve yield $^.player.test"
+        resp = self.execute_query(stmt)
+        self.check_resp_failed(resp)
+
+        stmt = "GO FROM 'Tim Duncan' OVER serve YIELD serve.test"
+        resp = self.execute_query(stmt)
+        self.check_resp_failed(resp)
+
+    def test_is_incall(self):
+        stmt = '''GO FROM 'Boris Diaw' OVER serve \
+            WHERE udf_is_in($$.team.name, 'Hawks', 'Suns') \
+            YIELD $^.player.name, serve.start_year, serve.end_year, $$.team.name'''
+        resp = self.execute_query(stmt)
+        self.check_resp_succeeded(resp)
+        expected_data = {
+            column_names : ["$^.player.name", "serve.start_year", "serve.end_year", "$$.team.name"],
             rows : [
                 []
             ]
