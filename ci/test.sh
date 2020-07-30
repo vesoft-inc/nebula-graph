913fe9d8af9bc1420d2e6972832e9751730d58c0
laura-ding <48548375+laura-ding@users.noreply.github.com>
2020-07-30 17:34:07 +0800

    Support empty and null value in ci test (#166)
    * support empty and null value
    
    * fix ci failed
    
    * update
    
    * modify type name
    
    * support regex
    
    * remove test code
    
    Co-authored-by: dutor <440396+dutor@users.noreply.github.com>

--- ci/test.sh
+++ ci/test.sh
@@ -83,11 +83,12 @@ function run_test() {
         $PROJ_DIR/tests/admin/* \
         $PROJ_DIR/tests/maintain/* \
         $PROJ_DIR/tests/mutate/* \
+        $PROJ_DIR/tests/query/v1/* \
         $PROJ_DIR/tests/query/stateless/test_schema.py \
         $PROJ_DIR/tests/query/stateless/test_if_exists.py \
         $PROJ_DIR/tests/query/stateless/test_range.py \
         $PROJ_DIR/tests/query/stateless/test_go.py \
-        $PROJ_DIR/tests/query/v1/*
+        $PROJ_DIR/tests/query/stateless/test_simple_query.py
 }
 
 case "$1" in
