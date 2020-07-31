cc30628939e2a7eb993581dd18e31dec773be7df
Shylock Hg <33566796+Shylock-Hg@users.noreply.github.com>
2020-07-31 13:26:43 +0800

    Check the columns name.

--- tests/query/v1/test_fetch_edges.py
+++ tests/query/v1/test_fetch_edges.py
@@ -22,43 +22,74 @@ class TestFetchEdges(NebulaTestSuite):
     def test_fetch_edges_base(self):
         query = 'FETCH PROP ON serve "Boris Diaw"->"Hawks" YIELD serve.start_year, serve.end_year'
         resp = self.execute_query(query)
+        expect_columns_name = ['serve._src',
+                               'serve._dst',
+                               'serve._rank',
+                               'serve.start_year',
+                               'serve.end_year']
         expect_result = [['Boris Diaw', 'Hawks', 0, 2003, 2005]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_columns_name)
         self.check_out_of_order_result(resp, expect_result)
 
         query = 'FETCH PROP ON serve "Boris Diaw"->"Hawks" YIELD serve.start_year > 2001, serve.end_year'
         resp = self.execute_query(query)
+        expect_columns_name = ['serve._src',
+                               'serve._dst',
+                               'serve._rank',
+                               '(serve.start_year>2001)',
+                               'serve.end_year']
         expect_result = [['Boris Diaw', 'Hawks', 0, True, 2005]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_columns_name)
         self.check_out_of_order_result(resp, expect_result)
 
         query = 'FETCH PROP ON serve "Boris Diaw"->"Hawks"@0 YIELD serve.start_year, serve.end_year'
         resp = self.execute_query(query)
+        expect_columns_name = ['serve._src',
+                               'serve._dst',
+                               'serve._rank',
+                               'serve.start_year',
+                               'serve.end_year']
         expect_result = [['Boris Diaw', 'Hawks', 0, 2003, 2005]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_columns_name)
         self.check_out_of_order_result(resp, expect_result)
 
         query = 'FETCH PROP ON serve "Boris Diaw"->"Hawks", "Boris Diaw"->"Suns" YIELD serve.start_year, serve.end_year'
         resp = self.execute_query(query)
+        expect_columns_name = ['serve._src',
+                               'serve._dst',
+                               'serve._rank',
+                               'serve.start_year',
+                               'serve.end_year']
         expect_result = [['Boris Diaw', 'Hawks', 0, 2003, 2005], ['Boris Diaw', 'Suns', 0, 2005, 2008]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_columns_name)
         self.check_out_of_order_result(resp, expect_result)
 
         query = '''GO FROM "Boris Diaw" OVER serve YIELD serve._src AS src, serve._dst AS dst \
                    | FETCH PROP ON serve $-.src->$-.dst YIELD serve.start_year, serve.end_year'''
         resp = self.execute_query(query)
+        expect_columns_name = ['serve._src',
+                               'serve._dst',
+                               'serve._rank',
+                               'serve.start_year',
+                               'serve.end_year']
         expect_result = [["Boris Diaw", "Hawks", 0, 2003, 2005],
                          ["Boris Diaw", "Suns", 0, 2005, 2008],
                          ["Boris Diaw", "Hornets", 0, 2008, 2012],
                          ["Boris Diaw", "Spurs", 0, 2012, 2016],
                          ["Boris Diaw", "Jazz", 0, 2016, 2017]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_columns_name)
         self.check_out_of_order_result(resp, expect_result)
 
         query = '''$a = GO FROM "Boris Diaw" OVER serve YIELD serve._src AS src, serve._dst AS dst; \
                    FETCH PROP ON serve $a.src->$a.dst YIELD serve.start_year, serve.end_year'''
         resp = self.execute_query(query)
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_columns_name)
         self.check_out_of_order_result(resp, expect_result)
 
         # TODO hash key
@@ -77,13 +108,20 @@ class TestFetchEdges(NebulaTestSuite):
     def test_fetch_edges_no_yield(self):
         query = 'FETCH PROP ON serve "Boris Diaw"->"Hawks"'
         resp = self.execute_query(query)
+        expect_columns_name = ['serve._src',
+                               'serve._dst',
+                               'serve._rank',
+                               'serve.start_year',
+                               'serve.end_year']
         expect_result = [['Boris Diaw', 'Hawks', 0, 2003, 2005]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_columns_name)
         self.check_out_of_order_result(resp, expect_result)
 
         query = 'FETCH PROP ON serve "Boris Diaw"->"Hawks"@0'
         resp = self.execute_query(query)
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_columns_name)
         self.check_out_of_order_result(resp, expect_result)
 
         # TODO hash key
@@ -103,42 +141,66 @@ class TestFetchEdges(NebulaTestSuite):
     def test_fetch_edges_distinct(self):
         query = 'FETCH PROP ON serve "Boris Diaw"->"Hawks", "Boris Diaw"->"Hawks" YIELD DISTINCT serve.start_year, serve.end_year'
         resp = self.execute_query(query)
+        expect_columns_name = ['serve._src',
+                               'serve._dst',
+                               'serve._rank',
+                               'serve.start_year',
+                               'serve.end_year']
         expect_result = [["Boris Diaw", "Hawks", 0, 2003, 2005]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_columns_name)
         self.check_out_of_order_result(resp, expect_result)
 
         query = '''GO FROM "Boris Diaw", "Boris Diaw" OVER serve YIELD serve._src AS src, serve._dst AS dst \
                    | FETCH PROP ON serve $-.src->$-.dst YIELD DISTINCT serve.start_year, serve.end_year'''
         resp = self.execute_query(query)
+        expect_columns_name = ['serve._src',
+                               'serve._dst',
+                               'serve._rank',
+                               'serve.start_year',
+                               'serve.end_year']
         expect_result = [["Boris Diaw", "Hawks", 0, 2003, 2005],
                          ["Boris Diaw", "Suns", 0, 2005, 2008],
                          ["Boris Diaw", "Hornets", 0, 2008, 2012],
                          ["Boris Diaw", "Spurs", 0, 2012, 2016],
                          ["Boris Diaw", "Jazz", 0, 2016, 2017]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_columns_name)
         self.check_out_of_order_result(resp, expect_result)
 
         query = '''$a = GO FROM "Boris Diaw", "Boris Diaw" OVER serve YIELD serve._src AS src, serve._dst AS dst; \
                    FETCH PROP ON serve $a.src->$a.dst YIELD DISTINCT serve.start_year, serve.end_year'''
         resp = self.execute_query(query)
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_columns_name)
         self.check_out_of_order_result(resp, expect_result)
 
         query = '''GO FROM "Tim Duncan", "Tony Parker" OVER serve YIELD serve._src AS src, serve._dst AS dst \
                    | FETCH PROP ON serve $-.src->$-.dst YIELD DISTINCT serve._dst as dst'''
         resp = self.execute_query(query)
+        expect_columns_name = ['serve._src',
+                               'serve._dst',
+                               'serve._rank',
+                               'dst']
         expect_result = [["Tim Duncan", "Spurs", 0, "Spurs"],
                          ["Tony Parker", "Spurs", 0, "Spurs"],
                          ["Tony Parker", "Hornets", 0, "Hornets"]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_columns_name)
         self.check_out_of_order_result(resp, expect_result)
 
     def test_fetch_edges_empty_input(self):
         query = '''GO FROM "not_exist_vertex" OVER serve YIELD serve._src AS src, serve._dst AS dst \
                    | FETCH PROP ON serve $-.src->$-.dst YIELD serve.start_year, serve.end_year'''
         resp = self.execute_query(query)
+        expect_columns_name = ['serve._src',
+                               'serve._dst',
+                               'serve._rank',
+                               'serve.start_year',
+                               'serve.end_year']
         expect_result = []
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_columns_name)
         self.check_out_of_order_result(resp, expect_result)
 
         query = '''GO FROM "Marco Belinelli" OVER serve YIELD serve._src AS src, serve._dst AS dst, serve.start_year as start \
@@ -147,6 +209,7 @@ class TestFetchEdges(NebulaTestSuite):
         resp = self.execute_query(query)
         expect_result = []
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_columns_name)
         self.check_out_of_order_result(resp, expect_result)
 
 
@@ -205,14 +268,27 @@ class TestFetchEdges(NebulaTestSuite):
     def test_fetch_edges_not_duplicate_column(self):
         query = 'FETCH PROP ON serve "Boris Diaw"->"Hawks" YIELD serve.start_year, serve.start_year'
         resp = self.execute_query(query)
+        expect_columns_name = ['serve._src',
+                               'serve._dst',
+                               'serve._rank',
+                               'serve.start_year',
+                               'serve.start_year']
         expect_result = [["Boris Diaw", "Hawks", 0, 2003, 2003]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_columns_name)
         self.check_out_of_order_result(resp, expect_result)
 
         query = 'FETCH PROP ON serve "Boris Diaw"->"Hawks" YIELD serve._src, serve._dst, serve._rank'
         resp = self.execute_query(query)
+        expect_columns_name = ['serve._src',
+                               'serve._dst',
+                               'serve._rank',
+                               'serve._src',
+                               'serve._dst',
+                               'serve._rank']
         expect_result = [["Boris Diaw", "Hawks", 0, "Boris Diaw", "Hawks", 0]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_columns_name)
         self.check_out_of_order_result(resp, expect_result)
 
         query = '''GO FROM "Boris Diaw" OVER serve YIELD serve._src AS src, serve._dst AS src \
