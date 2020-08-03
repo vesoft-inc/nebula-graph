74f07de3500192e3461a24d927d7cc9e0de57952
nevermore <58863790@qq.com>
2020-08-03 10:18:11 +0800

    add go_test_py (#149)
    * add go test
    
    * add case
    
    * more case
    
    * add more case
    
    * implement go_test
    
    * fix test result
    
    * change null to T_NULL
    
    Co-authored-by: jimingquan <mingquan.ji@vesoft.com>
    Co-authored-by: dutor <440396+dutor@users.noreply.github.com>

--- ci/test.sh
+++ ci/test.sh
@@ -83,6 +83,7 @@ function run_test() {
         $PROJ_DIR/tests/admin/* \
         $PROJ_DIR/tests/maintain/* \
         $PROJ_DIR/tests/mutate/* \
+        $PROJ_DIR/tests/query/stateless/test_new_go.py \
         $PROJ_DIR/tests/query/v1/* \
         $PROJ_DIR/tests/query/stateless/test_schema.py \
         $PROJ_DIR/tests/query/stateless/test_if_exists.py \
