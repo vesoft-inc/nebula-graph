88a42f661118f815c03da193d5582cac74896d91
Shylock Hg <33566796+Shylock-Hg@users.noreply.github.com>
2020-08-04 18:03:43 +0800

    Check the result column names.

--- tests/query/stateless/test_fetch_vertex.py
+++ tests/query/stateless/test_fetch_vertex.py
@@ -7,28 +7,35 @@
 
 from query.stateless.prepare_data import PrepareData
 
+# using _vid instead VertexID for vertex id column name not compatible to 1.0
 class TestFetchQuery(PrepareData):
     def test_fetch_vertex_base(self):
         query = 'FETCH PROP ON player "Boris Diaw" YIELD player.name, player.age'
         resp = self.execute_query(query)
+        expect_column_names = ['_vid', 'player.name', 'player.age']
         expect_result = [['Boris Diaw', 'Boris Diaw', 36]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_column_names)
         self.check_out_of_order_result(resp.rows, expect_result)
 
         query = 'FETCH PROP ON player "Boris Diaw" YIELD player.name, player.age, player.age > 30'
         resp = self.execute_query(query)
+        expect_column_names = ['_vid', 'player.name', 'player.age', '(player.age>30)']
         expect_result = [['Boris Diaw', 'Boris Diaw', 36, False]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_column_names)
         self.check_out_of_order_result(resp.rows, expect_result)
 
         query = '''GO FROM "Boris Diaw" over like YIELD like._dst as id
             | FETCH PROP ON player $-.id YIELD player.name, player.age'''
         resp = self.execute_query(query)
+        expect_column_names = ['_vid', 'player.name', 'player.age']
         expect_result = [
             ['Tony Parker', 'Tony Parker', 36],
             ['Tony Parker', 'Tim Duncan', 42]
         ]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_column_names)
         self.check_out_of_order_result(resp.rows, expect_result)
 
         query = '''$var = GO FROM "Boris Diaw" over like YIELD like._dst as id;
@@ -39,17 +46,20 @@ class TestFetchQuery(PrepareData):
             ['Tony Parker', 'tim duncan', 42]
         ]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_column_names)
         self.check_out_of_order_result(resp.rows, expect_result)
 
         query = '''$var = GO FROM %ld over like YIELD like._dst as id;
             FETCH PROP ON player $var.id YIELD player.name as name, player.age
             | ORDER BY name'''
         resp = self.execute_query(query)
+        expect_column_names = ['_vid', 'name', 'player.age']
         expect_result = [
             ['Tony Parker', 'Tony Parker', 36],
             ['Tim Duncan', 'Tim Duncan', 42]
         ]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_column_names)
         self.check_out_of_order_result(resp.rows, expect_result)
 
         # TODO insert hash key
@@ -69,8 +79,10 @@ class TestFetchQuery(PrepareData):
     def test_fetch_vertex_no_yield(self):
         query = 'FETCH PROP ON player "Boris Diaw"'
         resp = self.execute_query(query)
+        expect_column_names = ['_vid', 'player.name', 'player.age']
         expect_result = [['Boris Diaw', 'Boris Diaw', 36]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_column_names)
         self.check_out_of_order_result(resp.rows, expect_result)
 
         # TODO insert hash key
@@ -90,14 +102,18 @@ class TestFetchQuery(PrepareData):
     def test_fetch_vertex_disctinct(self):
         query = 'FETCH PROP ON player "Boris Diaw", "Boris Diaw" YIELD DISTINCT player.name, player.age'
         resp = self.execute_query(query)
+        expect_column_names = ['_vid', 'player.name', 'player.age']
         expect_result = [['Boris Diaw', 'Boris Diaw', 36]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_column_names)
         self.check_out_of_order_result(resp.rows, expect_result)
 
         query = 'FETCH PROP ON player "Boris Diaw", "Tony Parker" YIELD DISTINCT player.name, player.age'
         resp = self.execute_query(query)
+        expect_column_names = ['_vid', 'player.name', 'player.age']
         expect_result = [['Boris Diaw', 'Boris Diaw', 36], ['Tony Parker', 'Tony Parker', 36]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_column_names)
         self.check_out_of_order_result(resp.rows, expect_result)
 
     def test_fetch_vertex_syntax_error(self):
@@ -126,33 +142,41 @@ class TestFetchQuery(PrepareData):
     def test_fetch_vertex_not_exist_vertex(self):
         query = 'FETCH PROP ON player "not_exist_vertex"'
         resp = self.execute_query(query)
-        self.check_resp_succeeded(resp)
+        expect_column_names = ['_vid', 'player.name', 'player.age']
         expected = []
+        self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_column_names)
         self.check_out_of_order_result(resp, expected)
 
         query = 'GO FROM "not_exist_vertex" OVER serve | FETCH PROP ON team $-'
         resp = self.execute_query(query)
-        self.check_resp_succeeded(resp)
+        expect_column_names = ['_vid', 'team.name']
         expected = []
+        self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_column_names)
         self.check_out_of_order_result(resp, expected)
 
         query = 'FETCH PROP ON * "not_exist_vertex"'
         resp = self.execute_query(query)
-        self.check_resp_succeeded(resp)
         expected = []
+        self.check_resp_succeeded(resp)
         self.check_out_of_order_result(resp, expected)
 
     def test_fetch_vertex_get_all(self):
         query = 'FETCH PROP ON * "Boris Diaw"'
         resp = self.execute_query(query)
+        expect_column_names = ['_vid', 'player.name', 'player.age']
         expect_result = [['Boris Diaw', 'Boris Diaw', 36]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_column_names)
         self.check_out_of_order_result(resp.rows, expect_result)
 
         query = 'FETCH PROP ON bachelor "Tim Duncan" '
         resp = self.execute_query(query)
+        expect_column_names = ['_vid', 'bachelor.name', 'bachelor.speciality']
         expect_result = [["Tim Duncan", "Tim Duncan", "psychology"]]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_column_names)
         self.check_out_of_order_result(resp.rows, expect_result)
 
         query = 'FETCH PROP ON * "Tim Duncan" '
@@ -164,8 +188,10 @@ class TestFetchQuery(PrepareData):
     def test_fetch_vertex_duplicate_column_names(self):
         query = 'FETCH PROP ON player "Boris Diaw" YIELD player.name, player.name'
         resp = self.execute_query(query)
+        expect_column_names = ['_vid', 'player.name', 'player.name']
         expect_result = [['Boris Diaw', 'Boris Diaw', 'Boris Diaw']]
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_column_names)
         self.check_out_of_order_result(resp.rows, expect_result)
 
         query = '''GO FROM "Boris Diaw" over like YIELD like._dst as id, like._dst as id
@@ -183,8 +209,10 @@ class TestFetchQuery(PrepareData):
         query = '''GO FROM "not_exist_vertex" over like YIELD like._dst as id
             | FETCH PROP ON player $-.id'''
         resp = self.execute_query(query)
+        expect_column_names = ['_vid', 'player.name', 'player.name']
         expect_result = []
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_column_names)
         self.check_out_of_order_result(resp.rows, expect_result)
 
         query = '''GO FROM "Marco Belinelli" over serve YIELD serve._dst as id, serve.start_year as start
@@ -193,4 +221,5 @@ class TestFetchQuery(PrepareData):
         resp = self.execute_query(query)
         expect_result = []
         self.check_resp_succeeded(resp)
+        self.check_column_names(resp, expect_column_names)
         self.check_out_of_order_result(resp.rows, expect_result)
