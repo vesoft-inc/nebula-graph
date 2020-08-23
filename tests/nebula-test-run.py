d5fd1b5df8097bd71623321eda0662bea10ba1c5
laura-ding <48548375+laura-ding@users.noreply.github.com>
2020-08-23 17:11:06 +0800

    Add detect service is ok and init configs

--- tests/nebula-test-run.py
+++ tests/nebula-test-run.py
@@ -12,7 +12,6 @@ import pytest
 import sys
 from time import localtime, strftime
 from pathlib import Path
-from pathlib import Path
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 NEBULA_HOME = TEST_DIR + '/../'
@@ -21,6 +20,7 @@ sys.path.insert(0, TEST_DIR)
 
 from tests.common.nebula_service import NebulaService
 from tests.common.load_test_data import LoadGlobalData
+from tests.common.configs import init_configs
 
 TEST_LOGS_DIR = os.getenv('NEBULA_TEST_LOGS_DIR')
 if TEST_LOGS_DIR is None or TEST_LOGS_DIR == "":
@@ -70,7 +70,12 @@ if __name__ == "__main__":
     nebula_svc = NebulaService(NEBULA_BUILD_DIR, NEBULA_SOURCE_DIR)
     stop_nebula = True
     error_code = 0
+    configs = None
     try:
+        # Init args
+        parser = init_configs()
+        (configs, args) = parser.parse_args()
+
         # Create the test result directory if it doesn't already exist.
         if not os.path.exists(RESULT_DIR):
             os.makedirs(RESULT_DIR)
@@ -85,26 +90,21 @@ if __name__ == "__main__":
 
         args.extend(list(commandline_args))
 
-        debug_log = True
-        if '--debug_log' in args:
-            if args[args.index('--debug_log') + 1].lower() == 'false':
-                debug_log = False
-
         nebula_ip = ''
         nebula_port = 0
-        if '--address' not in args:
+        if len(configs.address) == 0:
             nebula_svc.install()
-            port = nebula_svc.start(debug_log)
+            port = nebula_svc.start(configs.debug_log)
             args.extend(['--address', '127.0.0.1:' + str(port)])
             nebula_ip = '127.0.0.1'
             nebula_port = port
         else:
             stop_nebula = False
-            addr = args[args.index('--address') + 1].split(':')
+            addr = configs.address.split(':')
             nebula_ip = addr[0]
             nebula_port = int(addr[1])
 
-        if '--data_dir' not in args:
+        if len(configs.data_dir) == 0:
             args.extend(['--data_dir', TEST_DIR])
         print("Running TestExecutor with args: {} ".format(args))
 
@@ -116,8 +116,11 @@ if __name__ == "__main__":
         os.chdir(TEST_DIR)
         error_code = executor.run_tests(args)
         load_data.drop_data()
+    except Exception as x:
+        print(x)
+
     finally:
-        if stop_nebula and pytest.cmdline.stop_nebula.lower() == 'true':
-            nebula_svc.stop(pytest.cmdline.rm_dir.lower() == 'true')
+        if stop_nebula and configs.stop_nebula.lower() == 'true':
+            nebula_svc.stop(configs.rm_dir.lower() == 'true')
 
     sys.exit(error_code)
