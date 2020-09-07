bf33acafe8bec2a719f5c345bc79cdaf18c40986
bright-starry-sky <56461666+bright-starry-sky@users.noreply.github.com>
2020-09-07 18:52:22 +0800

    change user email

--- src/optimizer/OptimizerUtils.cpp
+++ src/optimizer/OptimizerUtils.cpp
@@ -7,130 +7,29 @@
 #include "optimizer/OptimizerUtils.h"
 namespace nebula {
 namespace graph {
-using PropertyType = nebula::meta::cpp2::PropertyType;
-
-static Value::Type toValueType(PropertyType type) {
-    switch (type) {
-        case PropertyType::BOOL :
-            return Value::Type::BOOL;
-        case PropertyType::INT64 :
-        case PropertyType::INT32 :
-        case PropertyType::INT16 :
-        case PropertyType::INT8 :
-        case PropertyType::TIMESTAMP :
-            return Value::Type::INT;
-        case PropertyType::VID :
-            return Value::Type::VERTEX;
-        case PropertyType::FLOAT :
-        case PropertyType::DOUBLE :
-            return Value::Type::FLOAT;
-        case PropertyType::STRING :
-        case PropertyType::FIXED_STRING :
-            return Value::Type::STRING;
-        case PropertyType::DATE :
-            return Value::Type::DATE;
-        case PropertyType::DATETIME :
-            return Value::Type::DATETIME;
-        case PropertyType::UNKNOWN :
-            return Value::Type::__EMPTY__;
-    }
-}
 
 Value OptimizerUtils::boundValue(const meta::cpp2::ColumnDef& col,
-                                 BoundValueType op,
+                                 BoundValueOperator op,
                                  const Value& v) {
     switch (op) {
-        case BoundValueType::GREATER_THAN : {
+        case BoundValueOperator::GREATER_THAN : {
             return boundValueWithGT(col, v);
         }
-        case BoundValueType::LESS_THAN : {
+        case BoundValueOperator::LESS_THAN : {
             return boundValueWithLT(col, v);
         }
-        case BoundValueType::MAX : {
+        case BoundValueOperator::MAX : {
             return boundValueWithMax(col, v);
         }
-        case BoundValueType::MIN : {
-            return Value(NullType::__NULL__);
+        case BoundValueOperator::MIN : {
+            return boundValueWithMin(col, v);
         }
     }
-}
-
-Value OptimizerUtils::doubleWithGT(DoubleFormat df) {
-    if (df.integral == 9 && df.decimal == 999999 && df.exponent == 308 && df.isPositiveExpon()) {
-        return df.isPositive() ? Value(df.toDouble()) : Value(double(0));
-    }
-    // double default precision is 6, so the max value is 999999 of decimal part.
-    if (df.decimal < 999999) {
-        df.decimal = df.decimal + 1;
-    } else if (df.integral < 9) {
-        df.integral = df.integral + 1;
-    } else if (df.isPositiveExpon()) {
-        df.exponent = df.exponent + 1;
-        df.decimal = 0;
-        df.integral = 1;
-    } else {
-        if (df.exponent > 0) {
-            df.exponent = df.exponent - 1;
-        } else {
-            df.exponSign = '+';
-            df.exponent = 1;
-        }
-        df.decimal = 0;
-        df.integral = 1;
-    }
-    return Value(df.toDouble());
-}
-
-Value OptimizerUtils::doubleWithLT(DoubleFormat df) {
-    if (df.integral == 1 && df.decimal == 0 && df.exponent == 308 && !df.isPositiveExpon()) {
-        return df.isPositive() ? Value(double(0)) : Value(df.toDouble());
-    }
-    if (df.decimal > 0) {
-        df.decimal = df.decimal - 1;
-    } else if (df.integral > 1) {
-        df.integral = df.integral - 1;
-    } else if (!df.isPositiveExpon()) {
-        df.exponent = df.exponent + 1;
-        df.decimal = 999999;
-        df.integral = 9;
-    } else {
-        if (df.exponent > 0) {
-            df.exponent = df.exponent - 1;
-        } else {
-            df.exponSign = '-';
-            df.exponent = 1;
-        }
-        df.decimal = 999999;
-        df.integral = 9;
-
-    }
-    return Value(df.toDouble());
-}
-
-Value OptimizerUtils::boundDoubleWithGT(DoubleFormat df) {
-    if (df.integral == 0 and df.decimal == 0) {
-        return Value(double(1.0E-308));
-    }
-    if (df.isPositive()) {
-        return doubleWithGT(df);
-    } else {
-        return doubleWithLT(df);
-    }
-}
-
-Value OptimizerUtils::boundDoubleWithLT(DoubleFormat df) {
-    if (df.integral == 0 and df.decimal == 0) {
-        return Value(double(-std::numeric_limits<double>::max()));
-    }
-    if (df.isPositive()) {
-        return doubleWithLT(df);
-    } else {
-        return doubleWithGT(df);
-    }
+    return Value(NullType::BAD_TYPE);
 }
 
 Value OptimizerUtils::boundValueWithGT(const meta::cpp2::ColumnDef& col, const Value& v) {
-    auto type = toValueType(col.get_type());
+    auto type = SchemaUtil::propTypeToValueType(col.get_type());
     switch (type) {
         case Value::Type::INT : {
             if (v.getInt() == std::numeric_limits<int64_t>::max()) {
@@ -140,8 +39,18 @@ Value OptimizerUtils::boundValueWithGT(const meta::cpp2::ColumnDef& col, const V
             }
         }
         case Value::Type::FLOAT : {
-            DoubleFormat df(v.getFloat());
-            return boundDoubleWithGT(df);
+            if (v.getFloat() > 0.0f) {
+                if (v.getFloat() == std::numeric_limits<double_t>::max()) {
+                    return v;
+                }
+            } else if (v.getFloat() == 0.0f) {
+                return Value(std::numeric_limits<double_t>::min());
+            } else {
+                if (v.getFloat() == -std::numeric_limits<double_t>::min()) {
+                    return Value(0.0f);
+                }
+            }
+            return v.getFloat() + 0.0000000000000001f;
         }
         case Value::Type::BOOL: {
             return v;
@@ -155,22 +64,35 @@ Value OptimizerUtils::boundValueWithGT(const meta::cpp2::ColumnDef& col, const V
             str.append(v.getStr());
             str.append(*col.get_type_length() - v.getStr().size(), '\0');
             std::vector<unsigned char> bytes(str.begin(), str.end());
-            for (size_t i = bytes.size() - 1; i >= 0; i--) {
-                if (bytes[i] < 255) {
-                    bytes[i] += 1;
+            for (size_t i = bytes.size(); i >= 1; i--) {
+                if (bytes[i-1] < 255) {
+                    bytes[i-1] += 1;
                     break;
                 }
             }
             return Value(std::string(bytes.begin(), bytes.end()));
         }
         case Value::Type::DATE : {
+            if (Date(std::numeric_limits<int16_t>::max(), 12, 31) == v.getDate()) {
+                return v.getDate();
+            } else if (Date() == v.getDate()) {
+                return Date(0, 1, 2);
+            }
             auto d = v.getDate();
             if (d.day < 31) {
-                d.day = d.day + 1;
-            } else if (d.month < 12) {
-                d.month = d.month + 1;
-            } else if (d.year < std::numeric_limits<int16_t>::max()) {
-                d.year = d.year + 1;
+                d.day += 1;
+            } else {
+                d.day = 1;
+                if (d.month < 12) {
+                    d.month += 1;
+                } else {
+                    d.month = 1;
+                    if (d.year < std::numeric_limits<int16_t>::max()) {
+                        d.year += 1;
+                    } else {
+                        return v.getDate();
+                    }
+                }
             }
             return Value(d);
         }
@@ -179,18 +101,38 @@ Value OptimizerUtils::boundValueWithGT(const meta::cpp2::ColumnDef& col, const V
             // Ignore the time zone.
             if (dt.microsec < std::numeric_limits<int32_t>::max()) {
                 dt.microsec = dt.microsec + 1;
-            } else if (dt.sec < 60) {
-                dt.sec = dt.sec + 1;
-            } else if (dt.minute < 60) {
-                dt.minute = dt.minute + 1;
-            } else if (dt.hour < 24) {
-                dt.hour = dt.hour + 1;
-            } else if (dt.day < 31) {
-                dt.day = dt.day + 1;
-            } else if (dt.month < 12) {
-                dt.month = dt.month + 1;
-            } else if (dt.year < std::numeric_limits<int16_t>::max()) {
-                dt.year = dt.year + 1;
+            } else {
+                dt.microsec = 1;
+                if (dt.sec < 60) {
+                    dt.sec += 1;
+                } else {
+                    dt.sec = 1;
+                    if (dt.minute < 60) {
+                        dt.minute += 1;
+                    } else {
+                        dt.minute = 1;
+                        if (dt.hour < 24) {
+                            dt.hour += 1;
+                        } else {
+                            dt.hour = 1;
+                            if (dt.day < 31) {
+                                dt.day += 1;
+                            } else {
+                                dt.day = 1;
+                                if (dt.month < 12) {
+                                    dt.month += 1;
+                                } else {
+                                    dt.month = 1;
+                                    if (dt.year < std::numeric_limits<int16_t>::max()) {
+                                        dt.year += 1;
+                                    } else {
+                                        return v.getDateTime();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
             return Value(dt);
         }
@@ -201,7 +143,7 @@ Value OptimizerUtils::boundValueWithGT(const meta::cpp2::ColumnDef& col, const V
 }
 
 Value OptimizerUtils::boundValueWithLT(const meta::cpp2::ColumnDef& col, const Value& v) {
-    auto type = toValueType(col.get_type());
+    auto type = SchemaUtil::propTypeToValueType(col.get_type());
     switch (type) {
         case Value::Type::INT : {
             if (v.getInt() == std::numeric_limits<int64_t>::min()) {
@@ -211,8 +153,16 @@ Value OptimizerUtils::boundValueWithLT(const meta::cpp2::ColumnDef& col, const V
             }
         }
         case Value::Type::FLOAT : {
-            DoubleFormat df(v.getFloat());
-            return boundDoubleWithLT(df);
+            if (v.getFloat() < 0.0f) {
+                if (v.getFloat() == -std::numeric_limits<double_t>::max()) {
+                    return v;
+                } else if (v.getFloat() == -std::numeric_limits<double_t>::min()) {
+                    return Value(0.0f);
+                }
+            } else if (v.getFloat() == 0.0f) {
+                return Value(-std::numeric_limits<double_t>::min());
+            }
+            return v.getFloat() - 0.0000000000000001f;
         }
         case Value::Type::BOOL: {
             return v;
@@ -226,42 +176,75 @@ Value OptimizerUtils::boundValueWithLT(const meta::cpp2::ColumnDef& col, const V
             str.append(v.getStr());
             str.append(*col.get_type_length() - v.getStr().size(), '\0');
             std::vector<unsigned char> bytes(str.begin(), str.end());
-            for (size_t i = bytes.size() - 1; i >= 0; i--) {
-                if (bytes[i] > 0) {
-                    bytes[i] -= 1;
+            for (size_t i = bytes.size(); i >= 1; i--) {
+                if (bytes[i-1] > 0) {
+                    bytes[i-1] -= 1;
                     break;
                 }
             }
             return Value(std::string(bytes.begin(), bytes.end()));
         }
         case Value::Type::DATE : {
+            if (Date() == v.getDate()) {
+                return v.getDate();
+            }
             auto d = v.getDate();
-            if (d.day > 0) {
-                d.day = d.day - 1;
-            } else if (d.month > 0) {
-                d.month = d.month - 1;
-            } else if (d.year > 0) {
-                d.year = d.year - 1;
+            if (d.day > 1) {
+                d.day -= 1;
+            } else {
+                d.day = 31;
+                if ( d.month > 1) {
+                    d.month -= 1;
+                } else {
+                    d.month = 12;
+                    if (d.year > 1) {
+                        d.year -= 1;
+                    } else {
+                        return v.getDate();
+                    }
+                }
             }
             return Value(d);
         }
         case Value::Type::DATETIME : {
+            if (DateTime() == v.getDateTime()) {
+                return v.getDateTime();
+            }
             auto dt = v.getDateTime();
-            // Ignore the time zone.
-            if (dt.microsec > 0) {
-                dt.sec = dt.sec - 1;
-            } else if (dt.sec > 0) {
-                dt.sec = dt.sec - 1;
-            } else if (dt.minute > 0) {
-                dt.minute = dt.minute - 1;
-            } else if (dt.hour > 0) {
-                dt.hour = dt.hour - 1;
-            } else if (dt.day > 0) {
-                dt.day = dt.day - 1;
-            } else if (dt.month > 0) {
-                dt.month = dt.month - 1;
-            } else if (dt.year > 0){
-                dt.year = dt.year - 1;
+            if (dt.microsec > 1) {
+                dt.microsec -= 1;
+            } else {
+                dt.microsec = std::numeric_limits<int32_t>::max();
+                if (dt.sec > 1) {
+                    dt.sec -= 1;
+                } else {
+                    dt.sec = 60;
+                    if (dt.minute > 1) {
+                        dt.minute -= 1;
+                    } else {
+                        dt.minute = 60;
+                        if (dt.hour > 1) {
+                            dt.hour -= 1;
+                        } else {
+                            dt.hour = 24;
+                            if (dt.day > 1) {
+                                dt.day -= 1;
+                            } else {
+                                dt.day = 31;
+                                if (dt.month > 1) {
+                                    dt.month -= 1;
+                                } else {
+                                    dt.month = 12;
+                                    if (dt.year > 1) {
+                                        dt.year -= 1;
+                                    } else {
+                                        return v.getDateTime();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
             return Value(dt);
         }
@@ -271,7 +254,7 @@ Value OptimizerUtils::boundValueWithLT(const meta::cpp2::ColumnDef& col, const V
 }
 
 Value OptimizerUtils::boundValueWithMax(const meta::cpp2::ColumnDef& col, const Value& v) {
-    auto type = toValueType(col.get_type());
+    auto type = SchemaUtil::propTypeToValueType(col.get_type());
     switch (type) {
         case Value::Type::INT : {
             return Value(std::numeric_limits<int64_t>::max());
@@ -288,7 +271,7 @@ Value OptimizerUtils::boundValueWithMax(const meta::cpp2::ColumnDef& col, const 
             }
             std::string str;
             str.reserve(*col.get_type_length());
-            str.append(*col.get_type_length(), 0xFF);
+            str.append(*col.get_type_length(), '\377');
             return Value(str);
         }
         case Value::Type::DATE : {
@@ -316,13 +299,13 @@ Value OptimizerUtils::boundValueWithMax(const meta::cpp2::ColumnDef& col, const 
 }
 
 Value OptimizerUtils::boundValueWithMin(const meta::cpp2::ColumnDef& col, const Value& v) {
-    auto type = toValueType(col.get_type());
+    auto type = SchemaUtil::propTypeToValueType(col.get_type());
     switch (type) {
         case Value::Type::INT : {
             return Value(std::numeric_limits<int64_t>::min());
         }
         case Value::Type::FLOAT : {
-            return Value(std::numeric_limits<double>::min());
+            return Value(-std::numeric_limits<double>::max());
         }
         case Value::Type::BOOL: {
             return v;
--- src/optimizer/OptimizerUtils.h
+++ src/optimizer/OptimizerUtils.h
@@ -7,6 +7,7 @@
 #define NEBULA_GRAPH_OPTIMIZER_OPTIMIZERUTILS_H_
 
 #include "common/base/Base.h"
+#include "util/SchemaUtil.h"
 #include <common/interface/gen-cpp2/meta_types.h>
 
 namespace nebula {
@@ -14,67 +15,18 @@ namespace graph {
 
 class OptimizerUtils {
 public:
-    enum class BoundValueType {
+    enum class BoundValueOperator {
         GREATER_THAN = 0,
         LESS_THAN,
         MAX,
         MIN,
     };
 
-    // double default precision is 6
-    struct DoubleFormat {
-        char sign = '+';
-        int8_t integral;
-        int32_t decimal;
-        char exponSign = '+';
-        int32_t exponent;
-
-        explicit DoubleFormat(const double& d) {
-            size_t offset = 0;
-            auto v = folly::stringPrintf("%E", d);
-            if (v[0] == '-') {
-                sign = '-';
-                offset += 1;
-            }
-            integral = std::stoi(v.substr(offset, offset + 1));
-            auto pos = v.find_first_of('E');
-            decimal = std::stoi(v.substr(offset + 2, pos - 1));
-            if (v[pos + 1] == '-') {
-                exponSign = '-';
-            }
-            exponent = std::abs(std::stoi(v.substr(pos + 1, v.size())));
-        }
-
-        double toDouble() {
-            auto v = folly::stringPrintf("%d.%-6dE%c%02d", integral, decimal, exponSign, exponent);
-            if (sign == '-') {
-                v = sign + v;
-            }
-            return std::strtod(v.c_str(), nullptr);
-        }
-
-        bool isPositive() {
-            return sign == '+';
-        }
-
-        bool isPositiveExpon() {
-            return exponSign == '+';
-        }
-    };
-
 public:
     OptimizerUtils() = delete;
 
-    static Value boundDoubleWithGT(DoubleFormat df);
-
-    static Value boundDoubleWithLT(DoubleFormat df);
-
-    static Value doubleWithGT(DoubleFormat df);
-
-    static Value doubleWithLT(DoubleFormat df);
-
     static Value boundValue(const meta::cpp2::ColumnDef& col,
-                            BoundValueType op,
+                            BoundValueOperator op,
                             const Value& v = Value());
 
     static Value boundValueWithGT(const meta::cpp2::ColumnDef& col, const Value& v);
