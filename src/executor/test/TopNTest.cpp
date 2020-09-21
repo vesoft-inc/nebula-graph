b43992f07d0d8da82d96258e50d03bde878c8307
jievince <38901892+jievince@users.noreply.github.com>
2020-09-21 17:32:24 +0800

    fix topn test

--- src/executor/test/TopNTest.cpp
+++ src/executor/test/TopNTest.cpp
@@ -55,8 +55,8 @@ TEST_F(TopNTest, topnOneColAsc) {
     expected.emplace_back(Row({18}));
     expected.emplace_back(Row({19}));
     expected.emplace_back(Row({20}));
-    std::vector<std::pair<std::string, OrderFactor::OrderType>> factors;
-    factors.emplace_back(std::make_pair("2", OrderFactor::OrderType::ASCEND));
+    std::vector<std::pair<size_t, OrderFactor::OrderType>> factors;
+    factors.emplace_back(std::make_pair(2, OrderFactor::OrderType::ASCEND));
     TOPN_RESUTL_CHECK("input_sequential", "topn_one_col_asc", false, factors, 0, 4, expected);
 }
 
@@ -66,7 +66,7 @@ TEST_F(TopNTest, topnOneColDes) {
     expected.emplace_back(Row({19}));
     expected.emplace_back(Row({18}));
     expected.emplace_back(Row({18}));
-    std::vector<std::pair<std::string, OrderFactor::OrderType>> factors;
+    std::vector<std::pair<size_t, OrderFactor::OrderType>> factors;
     factors.emplace_back(std::make_pair(2, OrderFactor::OrderType::DESCEND));
     TOPN_RESUTL_CHECK("input_sequential", "topn_one_col_des", false, factors, 2, 9, expected);
 }
@@ -76,7 +76,7 @@ TEST_F(TopNTest, topnTwoColsAscAsc) {
     expected.emplace_back(Row({19, 2009}));
     expected.emplace_back(Row({20, 2008}));
     expected.emplace_back(Row({20, 2009}));
-    std::vector<std::pair<std::string, OrderFactor::OrderType>> factors;
+    std::vector<std::pair<size_t, OrderFactor::OrderType>> factors;
     factors.emplace_back(std::make_pair(2, OrderFactor::OrderType::ASCEND));
     factors.emplace_back(std::make_pair(4, OrderFactor::OrderType::ASCEND));
     TOPN_RESUTL_CHECK("input_sequential", "topn_two_cols_asc_asc", true, factors, 2, 3, expected);
@@ -86,7 +86,7 @@ TEST_F(TopNTest, topnTwoColsAscDes) {
     DataSet expected({"age", "start_year"});
     expected.emplace_back(Row({18, 2010}));
     expected.emplace_back(Row({18, 2010}));
-    std::vector<std::pair<std::string, OrderFactor::OrderType>> factors;
+    std::vector<std::pair<size_t, OrderFactor::OrderType>> factors;
     factors.emplace_back(std::make_pair(2, OrderFactor::OrderType::ASCEND));
     factors.emplace_back(std::make_pair(4, OrderFactor::OrderType::DESCEND));
     TOPN_RESUTL_CHECK("input_sequential", "topn_two_cols_asc_des", true, factors, 0, 2, expected);
@@ -94,7 +94,7 @@ TEST_F(TopNTest, topnTwoColsAscDes) {
 
 TEST_F(TopNTest, topnTwoColDesDes) {
     DataSet expected({"age", "start_year"});
-    std::vector<std::pair<std::string, OrderFactor::OrderType>> factors;
+    std::vector<std::pair<size_t, OrderFactor::OrderType>> factors;
     factors.emplace_back(std::make_pair(2, OrderFactor::OrderType::DESCEND));
     factors.emplace_back(std::make_pair(4, OrderFactor::OrderType::DESCEND));
     TOPN_RESUTL_CHECK("input_sequential", "topn_two_cols_des_des", true, factors, 10, 5, expected);
@@ -107,7 +107,7 @@ TEST_F(TopNTest, topnTwoColDesAsc) {
     expected.emplace_back(Row({19, 2009}));
     expected.emplace_back(Row({18, 2010}));
     expected.emplace_back(Row({18, 2010}));
-    std::vector<std::pair<std::string, OrderFactor::OrderType>> factors;
+    std::vector<std::pair<size_t, OrderFactor::OrderType>> factors;
     factors.emplace_back(std::make_pair(2, OrderFactor::OrderType::DESCEND));
     factors.emplace_back(std::make_pair(4, OrderFactor::OrderType::ASCEND));
     TOPN_RESUTL_CHECK("input_sequential", "topn_two_cols_des_asc", true, factors, 1, 9, expected);
