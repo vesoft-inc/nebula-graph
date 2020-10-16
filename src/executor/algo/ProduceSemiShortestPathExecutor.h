7035a57224d8cdd45fb370ebd874e1a49d7411ed
jimingquan <mingquan.ji@vesoft.com>
2020-10-16 11:15:29 +0800

    produce semi shortest path

---
+++ src/executor/algo/ProduceSemiShortestPathExecutor.h
@@ -0,0 +1,41 @@
+/* Copyright (c) 2020 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License,
+ * attached with Common Clause Condition 1.0, found in the LICENSES directory.
+ */
+
+#ifndef EXECUTOR_QUERY_PRODUCESEMISHORTESTPATHEXECUTOR_H_
+#define EXECUTOR_QUERY_PRODUCESEMISHORTESTPATHEXECUTOR_H_
+
+#include "executor/Executor.h"
+
+namespace nebula {
+namespace graph {
+class ProduceSemiShortestPathExecutor final : public Executor {
+public:
+    ProduceSemiShortestPathExecutor(const PlanNode* node, QueryContext* qctx)
+        : Executor("ProduceSemiShortestPath", node, qctx) {
+            setStartsVid();
+        }
+
+    folly::Future<Status> execute() override;
+
+    void setStartsVid();
+
+private:
+    void updateSrcDstCostPath(const Value& src, const Edge& edge);
+    void updateJumpDstCostPath(const Edge& edge);
+
+private:
+    // origin vids
+    std::vector<Value> starts_;
+    //  src : {dst : <cost, path>}
+    std::unordered_map<Value, std::unordered_map<Value, std::pair<int64_t, std::shared_ptr<Path>>>>
+        costPathMap_;
+
+    // std::unique_ptr<IWeight> weight_;
+};
+
+}   // namespace graph
+}   // namespace nebula
+#endif  // EXECUTOR_QUERY_PRODUCESEMISHORTESTPATHEXECUTOR_H_
