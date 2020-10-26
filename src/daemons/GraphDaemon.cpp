b0e20060f0455529464c6ea547c22483e821b5bf
Shylock Hg <33566796+Shylock-Hg@users.noreply.github.com>
2020-10-26 11:26:03 +0800

    Log the error when initialize failed.

--- src/daemons/GraphDaemon.cpp
+++ src/daemons/GraphDaemon.cpp
@@ -98,6 +98,7 @@ int main(int argc, char *argv[]) {
     // won't affect the process timezone.
     status = nebula::time::TimeUtils::initializeGlobalTimezone();
     if (!status.ok()) {
+        LOG(ERROR) << status;
         return EXIT_FAILURE;
     }
 
