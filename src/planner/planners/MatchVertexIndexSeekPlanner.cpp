0e72f0b185f6a6e386a44ce45d34702718284308
William Chen <13495049+CPWstatic@users.noreply.github.com>
2020-10-28 11:05:45 +0800

    Add match vertex index seek planner.

---
+++ src/planner/planners/MatchVertexIndexSeekPlanner.cpp
@@ -0,0 +1,23 @@
+/* Copyright (c) 2020 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License,
+ * attached with Common Clause Condition 1.0, found in the LICENSES directory.
+ */
+
+#include "planner/planners/MatchVertexIndexSeekPlanner.h"
+
+namespace nebula {
+namespace graph {
+MatchVertexIndexSeekPlannerRegister MatchVertexIndexSeekPlannerRegister::instance_;
+
+bool MatchVertexIndexSeekPlanner::match(AstContext* astCtx) {
+    UNUSED(astCtx);
+    return false;
+}
+
+StatusOr<SubPlan> MatchVertexIndexSeekPlanner::transform(AstContext* astCtx) {
+    UNUSED(astCtx);
+    return Status::Error();
+}
+}  // namespace graph
+}  // namespace nebula
