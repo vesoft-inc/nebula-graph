8db251a49d8beb4acd08c3161e69cda2c3a7bc31
jimingquan <mingquan.ji@vesoft.com>
2020-12-15 23:09:11 +0800

    add algo explain

--- src/planner/Algo.cpp
+++ src/planner/Algo.cpp
@@ -9,6 +9,45 @@
 namespace nebula {
 namespace graph {
 
+
+std::unique_ptr<PlanNodeDescription> ConjunctPath::explain() const {
+    auto desc = BiInputNode::explain();
+    switch (pathKind_) {
+        case PathKind::kBiBFS: {
+            addDescription("kind", "BFS", desc.get());
+            break;
+        }
+        case PathKind::kBiDijkstra: {
+            addDescription("kind", "Dijkstra", desc.get());
+            break;
+        }
+        case PathKind::kFloyd: {
+            addDescription("kind", "Floyd", desc.get());
+            break;
+        }
+        case PathKind::kAllPaths: {
+            addDescription("kind", "AllPath", desc.get());
+            break;
+        }
+    }
+    addDescription("conditionalVar", util::toJson(conditionalVar_), desc.get());
+    addDescription("noloop", util::toJson(noLoop_), desc.get());
+    return desc;
+}
+
+std::unique_ptr<PlanNodeDescription> ProduceAllPaths::explain() const {
+    auto desc = SingleDependencyNode::explain();
+    addDescription("noloop ", util::toJson(noLoop_), desc.get());
+    return desc;
+}
+
+std::unique_ptr<PlanNodeDescription> CartesianProduct::explain() const {
+    auto desc = SingleDependencyNode::explain();
+    addDescription("vars", folly::toJson(inputVar()), desc.get());
+    addDescription("colNames", folly::toJson(allColNames_), desc.get());
+    return desc;
+}
+
 Status CartesianProduct::addVar(std::string varName) {
     auto checkName = [&varName](auto var) { return var->name == varName; };
     if (std::find_if(inputVars_.begin(), inputVars_.end(), checkName) != inputVars_.end()) {
