fa88af484c912664463caa387cdc548e26ccd078
jimingquan <mingquan.ji@vesoft.com>
2021-01-05 18:49:10 +0800

    remove useless code

--- src/executor/query/AssignExecutor.cpp
+++ src/executor/query/AssignExecutor.cpp
@@ -13,8 +13,6 @@ namespace graph {
 folly::Future<Status> AssignExecutor::execute() {
     SCOPED_TIMER(&execTime_);
     auto* assign = asNode<Assign>(node());
-    // auto varName = assign->varName();
-    // auto* valueExpr = assign->valueExpr();
     auto& items = assign->items();
     QueryExpressionContext ctx(ectx_);
 
@@ -25,10 +23,6 @@ folly::Future<Status> AssignExecutor::execute() {
         ectx_->setValue(varName, std::move(value));
         VLOG(1) << "VarName is: " << varName << " value is : " << value;
     }
-
-    // auto value = valueExpr->eval(ctx);
-    // VLOG(1) << "VarName is: " << varName << " value is : " << value;
-    // ectx_->setValue(varName, std::move(value));
     DataSet ds;
     return finish(ResultBuilder().value(Value(std::move(ds))).finish());
 }
