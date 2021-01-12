2a8a711d1111fedf4f71549339f0eb6e6491b686
jievince <38901892+jievince@users.noreply.github.com>
2021-01-12 11:37:52 +0800

    remove clone id_

--- src/planner/PlanNode.h
+++ src/planner/PlanNode.h
@@ -232,7 +232,6 @@ protected:
 
     void clone(const PlanNode &node) {
         // TODO maybe shall copy cost_ and dependencies_ too
-        id_ = node.id_;
         inputVars_ = node.inputVars_;
         outputVars_ = node.outputVars_;
     }
