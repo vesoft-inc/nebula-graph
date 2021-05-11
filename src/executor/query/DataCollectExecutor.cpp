aba279203ed3524b38cf0f9a0cfe51f096227029
jimingquan <mingquan.ji@vesoft.com>
2021-05-11 19:32:37 +0800

    datacollect pathprop

--- src/executor/query/DataCollectExecutor.cpp
+++ src/executor/query/DataCollectExecutor.cpp
@@ -265,5 +265,75 @@ Status DataCollectExecutor::collectMultiplePairShortestPath(const std::vector<st
     return Status::OK();
 }
 
+Status DataCollectExecutor::collectPathProp(const std::vector<std::string>& vars) {
+    DataSet ds;
+    ds.colNames = colNames_;
+    DCHECK(!ds.colNames.empty());
+    // 0: vertices's props, 1: Edges's props 2: paths without prop
+    DCHECK_EQ(vars.size(), 3);
+
+    auto vIter = ectx_->getResult(vars[0]).iter();
+    std::unordered_map<Value, Vertex> vertexMap;
+    vertexMap.reserve(vIter->size());
+    DCHECK(vIter->isPropIter());
+    for (; vIter->valid(); vIter->next()) {
+        auto& vertexVal = vIter->getVertex();
+        if (!vertexVal.isVertex()) {
+            continue;
+        }
+        auto& vertex = vertexVal.getVertex();
+        vertexMap.insert(std::make_pair(vertex.vid, std::move(vertex)));
+    }
+
+    auto& eIter = ectx_->getResult(vars[1]).iter();
+    std::unordered_map<std::tuple<Value, EdgeType, EdgeRanking, Value>, Edge> edgeMap;
+    edgeMap.reserve(eIter->size());
+    DCHECK(eIter->isPropIter());
+    for (; eIter->valid(); eIter->next()) {
+        auto edgeVal = eIter->getEdge();
+        if (!edgeVal.isEdge()) {
+            continue;
+        }
+        auto& edge = edgeVal.getEdge();
+        auto edgeKey = std::make_tuple(edge.src, edge.type, edge.ranking, edge.dst);
+        edgeMap.insert(std::make_pair(std::move(edgeKey), std::move(edge)));
+    }
+
+    auto& pIter = ectx_->getResult(vars[2]).iter();
+    DCHECK(pIter->isSequentialIter());
+    for (; pIter->valid(); pIter->next()) {
+        auto pathVal = pIter->getColumn(0);
+        if (!pathVal.isPath()) {
+            continue;
+        }
+        auto& path = pathVal.getPath();
+        auto src = path.src.vid;
+        auto found = vertexMap.find(src);
+        if (found != vertexMap.end()) {
+            path.src = found->second;
+        }
+        for (auto& step : path.steps) {
+            auto dst = step.dst.vid;
+            step.dst = vertexMap[dst];
+
+            auto type = step.type;
+            auto ranking = step.ranking;
+            if (type < 0) {
+                dst = src;
+                src = step.dst.vid;
+                type = -type;
+            }
+            auto edgeKey = std::make_tuple(src, type, ranking, dst);
+            auto edge = edgeMap[edgeKey];
+            step.props = edge.props;
+            src = step.dst.vid;
+        }
+        ds.rows.emplace_back(Row({std::move(path)}));
+    }
+    VLOG(2) << "Path with props : \n" << ds;
+    result_.setDataSet(std::move(ds));
+    return Status::OK();
+}
+
 }  // namespace graph
 }  // namespace nebula
