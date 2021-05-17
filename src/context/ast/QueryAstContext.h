8dec40dbc6ce4dc59f92d914bc27f22b2340d3c4
jimingquan <mingquan.ji@vesoft.com>
2021-05-17 16:12:55 +0800

    fix error

--- src/context/ast/QueryAstContext.h
+++ src/context/ast/QueryAstContext.h
@@ -15,57 +15,60 @@ namespace nebula {
 namespace graph {
 
 enum FromType {
-    kInstant,
+    kInstantExpr,
     kVariable,
     kPipe,
 };
 
 struct Starts {
-    FromType fromType{kInstant};
-    Expression* src{nullptr};
-    Expression* originalSrc{nullptr};
-    std::string userDefinedVarName;
-    std::string firstBeginningSrcVidColName;
-    std::vector<Value> vids;
+    FromType                fromType{kInstantExpr};
+    Expression*             src{nullptr};
+    Expression*             originalSrc{nullptr};
+    std::string             userDefinedVarName;
+    std::string             firstBeginningSrcVidColName;
+    std::vector<Value>      vids;
 };
 
 struct Over {
-    bool isOverAll{false};
-    std::vector<EdgeType> edgeTypes;
-    storage::cpp2::EdgeDirection direction;
-    std::vector<std::string> allEdges;
+    bool                            isOverAll{false};
+    std::vector<EdgeType>           edgeTypes;
+    storage::cpp2::EdgeDirection    direction;
+    std::vector<std::string>        allEdges;
 };
 
 // path context
 struct PathContext final : AstContext {
-    Starts from;
-    Starts to;
-    StepClause steps;
-    Over over;
+    Starts          from;
+    Starts          to;
+    StepClause      steps;
+    Over            over;
 
     /*
     * find path from A to B OR find path from $-.src to $-.dst
     * fromVidsVar's DataSet save A OR $-.src
     * toVidsVar's DataSet save B OR $-.dst
     */
-    std::string fromVidsVar;
-    std::string toVidsVar;
+    std::string     fromVidsVar;
+    std::string     toVidsVar;
 
-    bool isShortest{false};
-    bool isWeight{false};
-    bool noLoop{false};
+    bool            isShortest{false};
+    bool            isWeight{false};
+    bool            noLoop{false};
 
-    // runtime
     /*
+    * runtime
     * find path from $-.src to $-.dst
     * project($-.src)<- dedup($-.src)
     * runtimeFromProject is project($-.src)
     * runtimeFromDedup is dedup($-.src)
     */
-    PlanNode* runtimeFromProject{nullptr};
-    PlanNode* runtimeFromDedup{nullptr};
-    PlanNode* runtimeToProject{nullptr};
-    PlanNode* runtimeToDedup{nullptr};
+    PlanNode*       runtimeFromProject{nullptr};
+    PlanNode*       runtimeFromDedup{nullptr};
+    PlanNode*       runtimeToProject{nullptr};
+    PlanNode*       runtimeToDedup{nullptr};
+    // just for pipe sentence,
+    // store the result of the previous sentence
+    std::string     inputVarName;
 };
 
 }  // namespace graph
