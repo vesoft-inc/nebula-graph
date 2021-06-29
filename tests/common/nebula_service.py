dcab165a8fb18ef23455cfb6d647f45a69fd56fa
Aiee <18348405+Aiee@users.noreply.github.com>
2021-06-29 15:40:52 +0800

    Revert --system_memory_high_watermark_ratio

--- tests/common/nebula_service.py
+++ tests/common/nebula_service.py
@@ -73,7 +73,7 @@ class NebulaService(object):
             params.append('--local_config=false')
             params.append('--enable_optimizer=true')
             params.append('--enable_authorize=true')
-            params.append('--system_memory_high_watermark_ratio=0.99')
+            params.append('--system_memory_high_watermark_ratio=0.95')
         if name == 'storaged':
             params.append('--local_config=false')
             params.append('--raft_heartbeat_interval_secs=30')
